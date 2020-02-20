8e14447d1077807b861c6d59ad6ce9164dc1b86a
Emir Causevic <emirc@ec-mbp-16.local>
2020-02-20 23:56:01 +0100

    deleted

--- spring-tutorial-2/core/src/main/java/com/kodectaacademy/numberguess/NumberGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.kodectaacademy.numberguess;
-
-public interface NumberGenerator {
-    int next();
-    int getMaxNumber();
-}
--- spring-tutorial-2/core/src/main/java/com/kodectaacademy/numberguess/NumberGuessMachine.java
+++
@@ -1,5 +0,0 @@
-package com.kodectaacademy.numberguess;
-
-public interface NumberGuessMachine {
-    int guess(int min, int max);
-}
