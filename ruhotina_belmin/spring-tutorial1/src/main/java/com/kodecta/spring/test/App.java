263deb9ac1148eca593ad5e1bc1bf517cdbc1431
beelmin <belmin.ruhotina@gmail.com>
2020-02-20 16:39:34 +0100

    novo

--- ruhotina_belmin/spring-tutorial1/src/main/java/com/kodecta/spring/test/App.java
+++ ruhotina_belmin/spring-tutorial1/src/main/java/com/kodecta/spring/test/App.java
@@ -1,15 +1,22 @@
 package com.kodecta.spring.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+
+
 public class App {
+    public static final Logger log = LoggerFactory.getLogger(App.class);
+
+
     public static void main(String[] args) {
 
         ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
 
-        //Person person = (Person) context.getBean("person");
-        //person.speak();
+        Person person = (Person) context.getBean("person");
+        person.speak();
 
         Tiger tiger = (Tiger) context.getBean("tiger_constructor");
         System.out.println(tiger.getName());
@@ -18,6 +25,8 @@ public class App {
         Tiger tiger1 = (Tiger) context.getBean("tiger_properties");
         System.out.println(tiger1.getName());
         System.out.println(tiger1.getColor());
+
+        log.debug(person.speak());
 
 
 
